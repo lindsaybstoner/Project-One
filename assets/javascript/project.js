d22284c08eba194465e739f44683b87b106f949e
Blank <ellenmacy@modv-ve500-0156.apn.wlan.upenn.edu>
2019-04-02 11:59:03 -0400

    last js changes

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -17,7 +17,8 @@ $(document).ready(function () {
         event.preventDefault();
 
         name = $('#artistInput').val().trim();
-        var artistName = name;
+        var artistName = name.toLowerCase();
+        
         rankRef.once("value", function (snapshot) {
             console.log(snapshot.val())
 
@@ -47,14 +48,42 @@ $(document).ready(function () {
 
     rankRef.orderByChild('popularity').limitToLast(3).on('value', function (snapshot) {
         var topThree = Object.values(snapshot.val());
-        
+
         for (var j = 0; j < topThree.length; j++) {
-            console.log(topThree[j].artist);
-            var printButtons =  $("<button>").text(topThree[j].artist);
+            //console.log(topThree[j].artist);
+            var printButtons = $("<button>").text(topThree[j].artist);
+            printButtons.attr("artist", topThree[j].artist);
+            printButtons.addClass("firebaseArtists");
             $("#buttons-view").append(printButtons);
         }
 
-    })
+    });
+
+
+    $(document.body).on("click", ".firebaseArtists", function (artist) {
+        event.preventDefault();
+
+        //console.log("hello");
+
+        let artistNameFire = $(this).attr("artist");
+        let firebaseArtist = artistNameFire.replace(" ", "+");
+        console.log(firebaseArtist);
+
+        getArtistInfo(firebaseArtist);
+        
+        //console.log(firebaseArtist);
+        getArtistBio(firebaseArtist);
+
+        $container.hide()
+        $('.home').contents().wrapAll('<div id="new-container">');
+        $newContainer.show()
+        document.body.style.backgroundImage = 'url(https://images.unsplash.com/photo-1470229722913-7c0e2dbbafd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80)';
+
+        artistInformation(firebaseArtist);
+        events(firebaseArtist);
+
+
+    });
 
 
     var $container = $(".container");
@@ -66,6 +95,9 @@ $(document).ready(function () {
         document.body.style.backgroundImage = 'url(https://images.unsplash.com/photo-1470229722913-7c0e2dbbafd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80)';
     }));
 
+   
+
+
 
     // Function to reset page when homeButton is clicked
     $("#homeButton").on("click", (function (event) {
@@ -75,7 +107,7 @@ $(document).ready(function () {
 
     function artistInformation(artist) {
 
-        var queryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
+        var queryURL = "https://cors-anywhere.herokuapp.com/https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -99,7 +131,7 @@ $(document).ready(function () {
     }
 
     function events(artist) {
-        var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+        var queryURL = "https://cors-anywhere.herokuapp.com/https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -132,185 +164,186 @@ $(document).ready(function () {
         events(inputArtist);
 
     });
-        })
-
-/** ----------------------------------------------------------------------------------- */
-
-$("#submitPress").on("click", function (event) {
-
-    let artist = $("#artistInput").val().trim();
-
-    let itunesArtist = artist.replace(" ", "+");
-    //console.log(itunesArtist);
-
-    getArtistInfo(itunesArtist);
-    getArtistBio(artist);
-})
-
-let song1 = "";
-let song2 = "";
-let song3 = "";
-
-function getArtistInfo(artist) {
 
 
-    $.ajax({
-        url: "https://itunes.apple.com/search",
-        data: { term: artist, media: "music" },
-        method: "POST"
-    }).then(function (response) {
+    /** ----------------------------------------------------------------------------------- */
 
-        const info = JSON.parse(response);
+    $("#submitPress").on("click", function (event) {
 
-        //var itunesArtistName = $("<h1>").text(info.results[0].artistName);
+        let artist = $("#artistInput").val().trim();
 
-        song1 = info.results[0].trackName;
-        //console.log(song1);
+        let itunesArtist = artist.replace(" ", "+");
+        //console.log(itunesArtist);
 
-        song2 = info.results[1].trackName;
-        //console.log(song2);
+        getArtistInfo(itunesArtist);
+        getArtistBio(artist);
+    })
 
-        song3 = info.results[2].trackName;
-        //console.log(song3);
+    let song1 = "";
+    let song2 = "";
+    let song3 = "";
 
-        artist = info.results[0].artistName;
-        //console.log(artist);
-
-        //var itunesArtistImage = $("<img>").attr("src", info.results[0].artworkUrl100);
-        var itunesSongName = "";
-        var itunesArtistAudio = "";
-        var lyricsButton = "";
-        //console.log(info.results[0]);
-
-        //$(".artist")
-        //  .append(itunesArtistName);
-
-        //artistImg(artist);
+    function getArtistInfo(artist) {
 
 
-        for (var i = 0; i < 3; i++) {
-            //itunesArtistImage = $("<img>").attr("src", info.results[i].artworkUrl100);
-            itunesSongName = $("<p class='song-name'>").text(info.results[i].trackName);
-            itunesArtistAudio = $("<audio controls>").attr("src", info.results[i].previewUrl);
-            lyricsButton = $("<button>").text("Get Lyrics");
+        $.ajax({
+            url: "https://itunes.apple.com/search",
+            data: { term: artist, media: "music" },
+            method: "POST"
+        }).then(function (response) {
 
-            lyricsButton
-                .attr("songTitle", info.results[i].trackName)
-                .attr("data-position", i)
-                .addClass("lyrics");
+            const info = JSON.parse(response);
 
+            //var itunesArtistName = $("<h1>").text(info.results[0].artistName);
 
-            $(`.song${i}`)
-                .append(itunesSongName)
-                .append(itunesArtistAudio)
-                .append(lyricsButton);
+            song1 = info.results[0].trackName;
+            console.log(song1);
 
+            song2 = info.results[1].trackName;
+            //console.log(song2);
 
+            song3 = info.results[2].trackName;
+            //console.log(song3);
 
-        }
+            artist = info.results[0].artistName;
+            //console.log(artist);
 
+            //var itunesArtistImage = $("<img>").attr("src", info.results[0].artworkUrl100);
+            var itunesSongName = "";
+            var itunesArtistAudio = "";
+            var lyricsButton = "";
+            //console.log(info.results[0]);
 
-        /** events(artist); */
-        getLyrics(artist);
+            //$(".artist")
+            //  .append(itunesArtistName);
 
-
-        return info;
-
-    });
-
-}
-
-
-
-function getLyrics(artist) {
-
-    $(".lyrics").on("click", function (event) {
-        event.preventDefault();
-
-        let song = $(this).attr("songTitle");
-        let dataPosition = $(this).attr('data-position');
-
-        if (!$(this).attr('data-gotLyrics')) {
-            $(this).attr('data-gotLyrics', 'true');
-            $(this).attr('data-isShown', 'true');
-
-            $.ajax({
-                url: `https://api.lyrics.ovh/v1/${artist}/${song}`,
-                method: "GET"
-            }).then(function (response) {
-                //console.log(response);
-
-                var printLyrics = $("<p>").text(response.lyrics);
-                printLyrics.addClass("lyricsPrinted")
-
-                $(".song" + dataPosition)
-                    .append(printLyrics);
+            //artistImg(artist);
 
 
-            });
+            for (var i = 0; i < 3; i++) {
+                //itunesArtistImage = $("<img>").attr("src", info.results[i].artworkUrl100);
+                itunesSongName = $("<p class='song-name'>").text(info.results[i].trackName);
+                itunesArtistAudio = $("<audio controls>").attr("src", info.results[i].previewUrl);
+                lyricsButton = $("<button>").text("Get Lyrics");
 
-            $(this).text("Hide Lyrics");
-        }
+                lyricsButton
+                    .attr("songTitle", info.results[i].trackName)
+                    .attr("data-position", i)
+                    .addClass("lyrics");
 
 
-        else {
-            if ($(this).attr('data-isShown') === "true") {
-                $(".song" + $(this).attr('data-position')).find(".lyricsPrinted").hide();
-                $(this).attr('data-isShown', 'false');
-                $(this).text("Get Lyrics");
+                $(`.song${i}`)
+                    .append(itunesSongName)
+                    .append(itunesArtistAudio)
+                    .append(lyricsButton);
+
+
 
             }
-            else {
-                $(".song" + $(this).attr('data-position')).find(".lyricsPrinted").show();
+
+
+            /** events(artist); */
+            getLyrics(artist);
+
+
+            return info;
+
+        });
+
+    }
+
+
+
+    function getLyrics(artist) {
+
+        $(".lyrics").on("click", function (event) {
+            event.preventDefault();
+
+            let song = $(this).attr("songTitle");
+            let dataPosition = $(this).attr('data-position');
+
+            if (!$(this).attr('data-gotLyrics')) {
+                $(this).attr('data-gotLyrics', 'true');
                 $(this).attr('data-isShown', 'true');
+
+                $.ajax({
+                    url: `https://api.lyrics.ovh/v1/${artist}/${song}`,
+                    method: "GET"
+                }).then(function (response) {
+                    //console.log(response);
+
+                    var printLyrics = $("<p>").text(response.lyrics);
+                    printLyrics.addClass("lyricsPrinted")
+
+                    $(".song" + dataPosition)
+                        .append(printLyrics);
+
+
+                });
+
                 $(this).text("Hide Lyrics");
             }
 
-        }
 
+            else {
+                if ($(this).attr('data-isShown') === "true") {
+                    $(".song" + $(this).attr('data-position')).find(".lyricsPrinted").hide();
+                    $(this).attr('data-isShown', 'false');
+                    $(this).text("Get Lyrics");
 
+                }
+                else {
+                    $(".song" + $(this).attr('data-position')).find(".lyricsPrinted").show();
+                    $(this).attr('data-isShown', 'true');
+                    $(this).text("Hide Lyrics");
+                }
 
-        //console.log(song);
-
-
-
-        //hideLyrics();
-    })
-}
-
-function hideLyrics() {
-    $(".lyrics").on("click", function (event) {
-        $(".lyricsPrinted").empty("");
-
-    });
-}
-
-
-function getArtistBio(artist) {
-    var queryURL = `https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${artist}`
-    //console.log(queryURL);
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        //console.log(response);
-        //console.log(response[1]);
-
-        let peopleWithLabel = response[1];
-
-        let hasMusicianLabel = false;
-
-        for (var i = 0; i < peopleWithLabel.length; i++) {
-            if (peopleWithLabel[i].includes('(musician)')) {
-                $("#bio").append($("<p>").text(response[2][i]));
-                hasMusicianLabel = true;
             }
-        }
-
-        if (!hasMusicianLabel) {
-            $("#bio").append($("<p>").text(response[2][0]));
-        }
 
 
-    })
-}
+
+            //console.log(song);
+
+
+
+            //hideLyrics();
+        })
+    }
+
+    function hideLyrics() {
+        $(".lyrics").on("click", function (event) {
+            $(".lyricsPrinted").empty("");
+
+        });
+    }
+
+
+    function getArtistBio(artist) {
+        var queryURL = `https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${artist}`
+        //console.log(queryURL);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            //console.log(response);
+            //console.log(response[1]);
+
+            let peopleWithLabel = response[1];
+
+            let hasMusicianLabel = false;
+
+            for (var i = 0; i < peopleWithLabel.length; i++) {
+                if (peopleWithLabel[i].includes('(musician)')) {
+                    $("#bio").append($("<p>").text(response[2][i]));
+                    hasMusicianLabel = true;
+                }
+            }
+
+            if (!hasMusicianLabel) {
+                $("#bio").append($("<p>").text(response[2][0]));
+            }
+
+
+        })
+    }
+});
